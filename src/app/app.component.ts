8a4b969c4ef7435f354c33a3a0056f603b16c9ad
Risshab Singla <risshab2singla@gmail.com>
2024-10-30 22:36:49 +0530

    Frames Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PlayerComponent } from './player/player.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, PlayerComponent, MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
